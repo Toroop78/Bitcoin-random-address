f89e0ced9ee312a72f8e482b9a43964dd095080b
Toroop78 <toroop7@yandex.ru>
2018-09-15 16:33:20 +0300

    Update a_random_key.go

--- a_random_key.go
+++ a_random_key.go
@@ -6,22 +6,24 @@ import (
      "github.com/btcsuite/btcd/btcec"
      "github.com/btcsuite/btcd/chaincfg"
 )
+
 func generateAddress() []byte {
 	padded := make([]byte, 32)
 	rand.Read(padded)
 	return padded
 }
-func main() {
-	for {
 
-		privkey, public := btcec.PrivKeyFromBytes(btcec.S256(), generateAddress())
-		wif, _ := btcutil.NewWIF(privkey, &chaincfg.MainNetParams, true)
+func main() {
+   for {
+
+	privkey, public := btcec.PrivKeyFromBytes(btcec.S256(), generateAddress())
+	wif, _ := btcutil.NewWIF(privkey, &chaincfg.MainNetParams, true)
         wifu, _ := btcutil.NewWIF(privkey, &chaincfg.MainNetParams, false)
 
-		caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
-		uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
+	caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
+	uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
 
-		fmt.Println(caddr.EncodeAddress(), wif.String())
-		fmt.Println(uaddr.EncodeAddress(), wifu.String())
-	}	
+	fmt.Println(caddr.EncodeAddress(), wif.String())
+	fmt.Println(uaddr.EncodeAddress(), wifu.String())
+    }	
 }
