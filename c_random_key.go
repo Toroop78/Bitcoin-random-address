bce39a1484ea0a0ee725739e9217465bcba013d2
Toroop78 <toroop7@yandex.ru>
2018-09-16 09:32:01 +0300

    Add files via upload

---
+++ c_random_key.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"crypto/rand"
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+func main() {
+
+	for {
+        padded := make([]byte, 32)
+	    rand.Read(padded)
+		
+		privkey, public := btcec.PrivKeyFromBytes(btcec.S256(), padded)
+		wif, _ := btcutil.NewWIF(privkey, &chaincfg.MainNetParams, true)
+        wifu, _ := btcutil.NewWIF(privkey, &chaincfg.MainNetParams, false)
+
+		caddr, _ := btcutil.NewAddressPubKey(public.SerializeCompressed(), &chaincfg.MainNetParams)
+		uaddr, _ := btcutil.NewAddressPubKey(public.SerializeUncompressed(), &chaincfg.MainNetParams)
+		
+		fmt.Println(caddr.EncodeAddress(), wif.String())
+		fmt.Println(uaddr.EncodeAddress(), wifu.String())	
+	}
+}
